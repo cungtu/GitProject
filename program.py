b9b11d423e33b37a8af0d993205ec6f8b4e5cc34
cungtu <ceeter1999@gmail.com>
2018-08-16 22:43:44 -0400

    moi

---
+++ program.py
@@ -0,0 +1,12 @@
+import stdio
+import sys
+
+# Accept a name as a command-line argument. Write a message containing
+# that name to standard output.
+
+stdio.write('Hi, ')
+stdio.write(sys.argv[1])
+stdio.writeln('. How are you?')
+
+
+
